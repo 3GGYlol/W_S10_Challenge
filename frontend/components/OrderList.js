3bff41a0174de5aa6f18ee3a411d8273eb72e56d
3GGYlol <xlj135@gmail.com>
2024-07-23 12:35:08 -0400

    wip

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -1,35 +1,74 @@
-import React from 'react'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useGetOrdersQuery } from "../state/apiSlice";
+import { setFilter } from "../state/filterSlice";
 
 export default function OrderList() {
-  const orders = []
+  const { data: orders } = useGetOrdersQuery();
+  const dispatch = useDispatch();
+  const currentSize = useSelector((state) => state.filter.size); 
+
   return (
-    <div id="orderList">
-      <h2>Pizza Orders</h2>
-      <ol>
-        {
-          orders.map(() => {
-            return (
-              <li key={1}>
-                <div>
-                  order details here
-                </div>
-              </li>
-            )
-          })
-        }
-      </ol>
-      <div id="sizeFilters">
-        Filter by size:
-        {
-          ['All', 'S', 'M', 'L'].map(size => {
-            const className = `button-filter${size === 'All' ? ' active' : ''}`
-            return <button
-              data-testid={`filterBtn${size}`}
-              className={className}
-              key={size}>{size}</button>
-          })
-        }
-      </div>
-    </div>
-  )
+  <div id="orderList">
+  <h2>Pizza Orders</h2>
+  <ol>
+  {currentSize === "All"
+  ? orders?.map((order) => {
+  return (
+  <li key={order.id}>
+   <div>
+   {order.customer} ordered a size {order.size} with
+   {order.toppings
+   ? order.toppings.map((topping, idx) => {
+   if (idx === order.toppings.length - 1) {
+  return ` ${idx + 1} ${
+   idx + 1 > 1 ? "toppings" : "topping"
+   }`;
+   }
+   })
+  : " no toppings"}
+   </div>
+   </li>
+      );
+    })
+    : orders?.map((order) => {
+    if (order.size === currentSize)
+    return (
+    <li key={order.id}>
+   <div>
+   {order.customer} ordered a size {order.size} with
+    {order.toppings
+   ? order.toppings.map((topping, idx) => {
+   if (idx === order.toppings.length - 1) {
+   return ` ${idx + 1} ${
+   idx + 1 > 1 ? "toppings" : "topping"
+   }`;
+  }
+})
+ : " no toppings"}
+ </div>
+</li>
+ );
+})}
+</ol>
+<div id="sizeFilters">
+Filter by size:
+{["All", "S", "M", "L"].map((size) => {
+const className = `button-filter${
+size === currentSize ? " active" : ""
+  }`;
+    return (
+     <button
+       onClick={() => dispatch(setFilter(size))}
+       data-testid={`filterBtn${size}`}
+        className={className}
+         key={size}
+        >
+      {size}
+    </button>
+    );
+  })}
+   </div>
+  </div>
+  );
 }
--- frontend/components/PizzaForm.js
+++ frontend/components/PizzaForm.js
@@ -1,65 +1,187 @@
-import React from 'react'
+import React, { useReducer } from "react";
+import { useAddOrderMutation } from "../state/apiSlice";
 
-const initialFormState = { // suggested
-  fullName: '',
-  size: '',
-  '1': false,
-  '2': false,
-  '3': false,
-  '4': false,
-  '5': false,
-}
+const initialFormState = {
+  fullName: "",
+  size: "",
+  1: false,
+  2: false,
+  3: false,
+  4: false,
+  5: false,
+};
+
+const CHANGE_INPUT = "CHANGE_INPUT";
+const RESET_FORM = "RESET_FORM";
+const CHANGE_SIZE = "CHANGE_SIZE";
+const CHANGE_TOPPING = "CHANGE_TOPPING";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CHANGE_INPUT: {
+      const { name, value } = action.payload;
+      return { ...state, [name]: value };
+    }
+    case CHANGE_SIZE: {
+      const { name, value } = action.payload;
+      return { ...state, [name]: value };
+    }
+    case CHANGE_TOPPING: {
+      const { name, checked } = action.payload;
+      return { ...state, [name]: checked };
+    }
+    case RESET_FORM:
+      return initialFormState;
+    default:
+      return state;
+  }
+};
 
 export default function PizzaForm() {
+  const [state, dispatch] = useReducer(reducer, initialFormState);
+  const [addOrder, { isLoading, error: orderError }] = useAddOrderMutation();
+
+  const onInputChange = ({ target: { name, value } }) => {
+    dispatch({ type: CHANGE_INPUT, payload: { name, value } });
+  };
+
+  const onSizeChange = ({ target: { name, value } }) => {
+    dispatch({ type: CHANGE_SIZE, payload: { name, value } });
+  };
+
+  const onToppingChange = ({ target: { name, checked } }) => {
+    dispatch({ type: CHANGE_TOPPING, payload: { name, checked } });
+  };
+
+  const resetForm = () => {
+    dispatch({ type: RESET_FORM });
+  };
+
+  const onOrderSubmit = async (evt) => {
+    evt.preventDefault();
+    const { fullName, size } = state;
+    const allToppings = [
+      state["1"],
+      state["2"],
+      state["3"],
+      state["4"],
+      state["5"],
+    ];
+    let toppings = [];
+    allToppings.forEach((topping, idx) => {
+      if (topping) {
+        toppings.push(idx + 1);
+      }
+    });
+    try {
+      await addOrder({ fullName, size, toppings }).unwrap();
+      resetForm();
+    } catch (err) {
+      console.error("Failed to add order: ", err);
+    }
+  };
+
   return (
-    <form>
-      <h2>Pizza Form</h2>
-      {true && <div className='pending'>Order in progress...</div>}
-      {true && <div className='failure'>Order failed: fullName is required</div>}
+    <div>
+      {isLoading && <div className="pending">Order in progress...</div>}
+      <form onSubmit={onOrderSubmit}>
+        <h2>Pizza Form</h2>
+        {orderError && <div className="failure">{orderError.data.message}</div>}
 
-      <div className="input-group">
-        <div>
-          <label htmlFor="fullName">Full Name</label><br />
-          <input
-            data-testid="fullNameInput"
-            id="fullName"
-            name="fullName"
-            placeholder="Type full name"
-            type="text"
-          />
+        <div className="input-group">
+          <div>
+            <label htmlFor="fullName">Full Name</label>
+            <br />
+            <input
+              data-testid="fullNameInput"
+              id="fullName"
+              name="fullName"
+              placeholder="Type full name"
+              type="text"
+              onChange={onInputChange}
+              value={state.fullName}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="input-group">
-        <div>
-          <label htmlFor="size">Size</label><br />
-          <select data-testid="sizeSelect" id="size" name="size">
-            <option value="">----Choose size----</option>
-            <option value="S">Small</option>
-            <option value="M">Medium</option>
-            <option value="L">Large</option>
-          </select>
+        <div className="input-group">
+          <div>
+            <label htmlFor="size">Size</label>
+            <br />
+            <select
+              data-testid="sizeSelect"
+              id="size"
+              name="size"
+              onChange={onSizeChange}
+              value={state.size}
+            >
+              <option value="">----Choose size----</option>
+              <option value="S">Small</option>
+              <option value="M">Medium</option>
+              <option value="L">Large</option>
+            </select>
+          </div>
         </div>
-      </div>
 
-      <div className="input-group">
-        <label>
-          <input data-testid="checkPepperoni" name="1" type="checkbox" />
-          Pepperoni<br /></label>
-        <label>
-          <input data-testid="checkGreenpeppers" name="2" type="checkbox" />
-          Green Peppers<br /></label>
-        <label>
-          <input data-testid="checkPineapple" name="3" type="checkbox" />
-          Pineapple<br /></label>
-        <label>
-          <input data-testid="checkMushrooms" name="4" type="checkbox" />
-          Mushrooms<br /></label>
-        <label>
-          <input data-testid="checkHam" name="5" type="checkbox" />
-          Ham<br /></label>
-      </div>
-      <input data-testid="submit" type="submit" />
-    </form>
-  )
+        <div className="input-group">
+          <label>
+            <input
+              data-testid="checkPepperoni"
+              name="1"
+              type="checkbox"
+              onChange={onToppingChange}
+              checked={state["1"]}
+            />
+            Pepperoni
+            <br />
+          </label>
+          <label>
+            <input
+              data-testid="checkGreenpeppers"
+              name="2"
+              type="checkbox"
+              onChange={onToppingChange}
+              checked={state["2"]}
+            />
+            Green Peppers
+            <br />
+          </label>
+          <label>
+            <input
+              data-testid="checkPineapple"
+              name="3"
+              type="checkbox"
+              onChange={onToppingChange}
+              checked={state["3"]}
+            />
+            Pineapple
+            <br />
+          </label>
+          <label>
+            <input
+              data-testid="checkMushrooms"
+              name="4"
+              type="checkbox"
+              onChange={onToppingChange}
+              checked={state["4"]}
+            />
+            Mushrooms
+            <br />
+          </label>
+          <label>
+            <input
+              data-testid="checkHam"
+              name="5"
+              type="checkbox"
+              onChange={onToppingChange}
+              checked={state["5"]}
+            />
+            Ham
+            <br />
+          </label>
+        </div>
+        <input data-testid="submit" type="submit" />
+      </form>
+    </div>
+  );
 }
